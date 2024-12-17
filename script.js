38f30b3ba07c74669d5fb04a0664cfad455eadcb
BVLPAVANI <154451965+BVLPAVANI@users.noreply.github.com>
2024-12-17 09:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-
-const addButton = document.getElementById("add");
-const todoInput = document.getElementById("input");
-const todoList = document.getElementById("list");
-
-addButton.addEventListener("click", () => {
-  const taskText = todoInput.value;
-
-  if (taskText === "") {
-    alert("Please enter a task!");
-    return;
-  }
+const addButton = document.querySelector("#add");
+const taskInput = document.querySelector("#input");
+const taskList = document.querySelector("#list");
+addButton.addEventListener("click", addTask);
+function addTask() {
+  const taskText = taskInput.value;
+function isInputEmpty(input) {
+  return input.trim() === ""; 
+}
+if (isInputEmpty(taskText)) {
+  alert("Please enter a task!");
+  return;
+}
+  const taskItem = createTaskElement(taskText);
+  taskList.appendChild(taskItem);
+  taskInput.value = "";
+}
+function createTaskElement(taskText) {
   const li = document.createElement("li");
   li.textContent = taskText;
-  todoList.appendChild(li);
-todoInput.value = "";
-});
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.addEventListener("click", () => li.remove());
+
+  li.appendChild(deleteButton);
+
+  return li;
+}
